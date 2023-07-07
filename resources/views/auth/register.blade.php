0500a3d097f4e86480c481a5b3631488fcb36a80
JosephAbubo <joseph.abubo031801@gmail.com>
2023-07-07 21:08:52 +0800

    added user type in registration

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -52,12 +52,19 @@
                                 @enderror
                             </div>
                         </div>
-
                         <div class="row mb-3">
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="User Type" class="col-md-4 col-form-label text-md-end">{{ __('Type') }}</label>
+
+                            <div class="col-md-6">
+                            <input id="type" type="text" class="form-control @error('type') is-invalid @enderror" name="type" value="{{ old('type') }}" required autocomplete="type" autofocus>
                             </div>
                         </div>
 
