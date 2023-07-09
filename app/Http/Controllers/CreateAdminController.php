f9ae87d09ea4bcf5d1f3d165519bf373183ac242
JosephAbubo <joseph.abubo031801@gmail.com>
2023-07-09 21:17:09 +0800

    create admin controller and form request

---
+++ app/Http/Controllers/CreateAdminController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Http\Requests\CreateAdminRequestForm;
+
+class CreateAdminController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(CreateAdminRequestForm $request)
+    {
+        $user = \App\Models\User::create($request->validated());
+        return $user;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $user->update($request->validated());
+        return $user;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return response()->noContent();
+    }
+}
